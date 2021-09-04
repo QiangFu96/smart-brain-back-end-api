bcfa99cf08250bbd6c8488d096a0719b3e642533
QiangFu96 <qf2143@columbia.edu>
2021-09-04 12:15:13 -0400

    adding port

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.put('/image', (req,res) => { image.handleImage(req,res,db)})
 //newend-point for api calls
 app.post('/imageurl', (req,res) => { image.handleApiCall(req,res)})
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 
