ecf0366ea3f3328b949f296c1daa281ac2a6e69d
QiangFu96 <qf2143@columbia.edu>
2021-09-04 12:41:02 -0400

    db connection

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
 	client:'pg',
 	connection: {
-		host: 'postgresql-spherical-42401',
-		user: 'qiangfu',
-		password:'',
-		database: 'smart-brain'
+		host: process.env.DATABASE_URL,
+		ssl:true
 	}
 });
 
